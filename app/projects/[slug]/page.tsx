3ae20ad060e288072490cb1abbe018d56141b69d
Vinícius Reis <viniciusreis3105@gmail.com>
2024-10-01 17:31:44 -0300

    feat: add breadcrumb in project slug page

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,6 +1,17 @@
 "use client";
 
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/app/_components/ui/breadcrumb";
 import { projects } from "@/app/_data/db";
+import { ArrowDown } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import { useParams } from "next/navigation";
 
 export default function ProjectPage() {
@@ -13,15 +24,30 @@ export default function ProjectPage() {
   }
 
   return (
-    <div className="container mx-auto max-w-screen-xl py-16">
-      <div className="flex flex-col gap-4">
-        <h3 className="text-base font-medium uppercase text-muted-foreground md:text-lg">
-          Projeto
-        </h3>
-        <h1 className="mb-5 text-3xl font-medium md:mb-0 md:text-5xl">
-          {project.name}
-        </h1>
-        <p className="text-lg text-muted-foreground">{project.description}</p>
+    <div className="relative h-[80vh] w-full bg-background">
+      {/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
+      <div className="absolute bottom-0 left-0 right-0 top-0 z-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+      <div className="container relative mx-auto max-w-screen-xl py-16">
+        <div className="flex items-center justify-between gap-10">
+          <div className="flex flex-col gap-2">
+            <Breadcrumb>
+              <BreadcrumbList className="text-lg">
+                <BreadcrumbItem>
+                  <BreadcrumbLink asChild>
+                    <Link href="/projects">Projetos</Link>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>{project.name}</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+          <ArrowDown size={32} className="hidden md:inline-flex" />
+        </div>
+        {/* banner image */}
+        <Image src="" alt="" />
       </div>
     </div>
   );
